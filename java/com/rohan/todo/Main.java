9c54e25bdc7873803aff7266ac83d943a033fc94
Rohan Mathur <rohanmathur34@gmail.com>
2013-05-20 21:13:40 -0400

    Initial Commit
    Current status:
    [*] Working add task functionality
    
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

---
+++ java/com/rohan/todo/Main.java
@@ -0,0 +1,71 @@
+package com.rohan.todo;
+
+import com.rohan.todo.R; //not unused as Android Studio says it is, needed to refer to the parts of the XML layout
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.ListView;
+import java.util.ArrayList;
+import android.widget.ArrayAdapter;
+
+public class Main extends Activity implements OnClickListener, OnKeyListener {
+
+    EditText txtItem;
+    Button btnAdd;
+    ListView listItems;
+
+    ArrayList<String> toDoItems;
+    ArrayAdapter<String> aa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        txtItem = (EditText)findViewById(R.id.txtItem);
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+        listItems = (ListView)findViewById(R.id.listItems);
+
+        btnAdd.setOnClickListener(this);
+        txtItem.setOnKeyListener(this);
+
+        toDoItems = new ArrayList<String>();
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, toDoItems);
+        listItems.setAdapter(aa);
+    }
+
+    private void addItem(String item){
+        if(item.length() > 0){
+            this.toDoItems.add(item);
+            this.aa.notifyDataSetChanged();
+            this.txtItem.setText("");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void onClick(View v){
+        if(v == this.btnAdd){
+            this.addItem(this.txtItem.getText().toString());
+        }
+    }
+
+    public boolean onKey(View v, int keyCode, KeyEvent event){
+        if(event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_DPAD_CENTER){
+            this.addItem(this.txtItem.getText().toString());
+        }
+        return false;
+    }
+
+}
